29ea5f2d8b77cdbda0bd3b5b17cfdbaf50915602
Smrity11 <kanisasia376@gmail.com>
2023-08-20 10:20:29 -0700

    solved some mejor problem ,big error

--- summer.js
+++ summer.js
@@ -1,64 +1,58 @@
+function discount() {
+    const total = parseFloat(document.getElementById('card-price').innerText);
+    const cupponInput = document.getElementById('cuppon-input');
+    const cupponBtn = document.getElementById('cuppon-btn');
 
+    if (total > 200) {
+        cupponBtn.removeAttribute('disabled');
 
-function discount(){
-    if(TotalPrice>200){
-        const inputValue= document.getElementById('cuppon-input').value
-        const cupponBtn =document.getElementById('cuppon-btn')
-        cupponBtn.removeAttribute('disabled')
-        
-            cupponBtn.addEventListener('click',function(){
-                if(inputValue=='SELL200'){
-                const cardDiscount =document.getElementById('card-discount')
-                
-                const discount =TotalPrice-((TotalPrice*20)/100)
-                const cardTotalTK =document.getElementById('card-total-price')
-                cardTotalTK.innerText=discount
-                cardDiscount.innerText=discount
-                
-                 }
-                
-            }) 
-        }
-    
-    else{
-        cupponBtn.setAttribute('disabled',true)
+        cupponBtn.addEventListener('click', function () {
+            const inputValue = cupponInput.value.trim();
+
+            if (inputValue === 'SELL200') {
+                const discount = total * 0.2;
+                const cardTotalTK = document.getElementById('card-total-price');
+                cardTotalTK.innerText = (total - discount).toFixed(2);
+                const cardDiscount = document.getElementById('card-discount');
+                cardDiscount.innerText = discount.toFixed(2);
+            }
+            cupponInput.value=' '
+        });
+    } else {
+        cupponBtn.setAttribute('disabled', true);
     }
 }
- 
-function finalTotal(total){
-   const purchasebtn =document.getElementById('purchase-btn')
-    if(total==0){
-        purchasebtn.setAttribute('disabled' , true)
+
+function finalTotal(total) {
+    const purchasebtn = document.getElementById('purchase-btn');
+    if (total == 0) {
+        purchasebtn.setAttribute('disabled', true);
+    } else {
+        purchasebtn.removeAttribute('disabled');
     }
-    else{
-        purchasebtn.removeAttribute('disabled')
-    }
-   
 }
 
-let total =0
-function clickCard(target){
-    
-    const cardName = target.childNodes[3].childNodes[3];
-    
-    const cardNmameText =cardName.innerText
-    const productNmameshow = document.getElementById('product-name-show')
-    const productNmameshowTag =document.createElement('p')
-   const count = productNmameshow.childElementCount
-   productNmameshowTag.innerHTML =`${count+1}. ${cardNmameText}`
-   productNmameshow.appendChild(productNmameshowTag)
+let total = 0;
 
-   const cardPriceElement = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
-   const price = parseInt(cardPriceElement);
-    total = total + price;
-    TotalPrice= document.getElementById('card-price').innerText = total;
+function clickCard(target) {
+    const cardName = target.querySelector('.card-title');
+    const cardPriceElement = target.querySelector('.text-sm').textContent.split(' ')[0];
+    const price = parseFloat(cardPriceElement);
 
-    const cardTotalTK =document.getElementById('card-total-price').innerText =total
-   
-    finalTotal(total)
-    discount()
-    
-    
+    total += price;
+
+    const productNameshow = document.getElementById('product-name-show');
+    const productNameshowTag = document.createElement('p');
+    const count = productNameshow.childElementCount;
+
+    productNameshowTag.innerHTML = `${count + 1}. ${cardName.textContent}`;
+    productNameshow.appendChild(productNameshowTag);
+
+    document.getElementById('card-price').innerText = total.toFixed(2);
+    document.getElementById('card-total-price').innerText = total.toFixed(2);
+
+    finalTotal(total);
+    discount();
 }
 
 document.getElementById('purchase-btn').addEventListener('click', function () {
@@ -67,22 +61,17 @@ document.getElementById('purchase-btn').addEventListener('click', function () {
     modal.showModal();
 });
 
-
 document.getElementById('modal-go-home-btn').addEventListener('click', function () {
-    
-    document.getElementById('product-name-show').innerHTML = '';
-
+    const productNameshow = document.getElementById('product-name-show');
+    productNameshow.innerHTML = '';
     total = 0;
-    document.getElementById('card-price').innerText = total;
-    document.getElementById('card-discount').innerText = total;
-    document.getElementById('card-total-price').innerText = total;
-
-    const cupponBtn =document.getElementById('cuppon-btn')
+    document.getElementById('card-price').innerText = '0.00';
+    document.getElementById('card-discount').innerText = '0.00';
+    document.getElementById('card-total-price').innerText = '0.00';
+    const cupponBtn = document.getElementById('cuppon-btn');
     cupponBtn.setAttribute('disabled', true);
     const purchaseBtn = document.getElementById('purchase-btn');
     purchaseBtn.setAttribute('disabled', true);
-
     const modal = document.getElementById('my_modal_1');
     modal.close();
 });
-
